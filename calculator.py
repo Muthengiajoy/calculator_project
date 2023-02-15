f1fa5744a67129df481dce0f0ed0ec70870559bd
Muthengiajoy <prof.joy7079@gmail.com>
2023-02-15 11:50:52 +0300

    operators

--- calculator.py
+++ calculator.py
@@ -1,19 +1,33 @@
 #this is a calculator project
-num1 = eval(input("enter_first_number:"))
-num2 = eval(input("enter_second_number:"))
+number1 = eval(input("enter_first_number:"))
+number2= eval(input("enter_second_number:"))
 operator = input("enter_operator:")
 def add(num1,num2):
     result = num1 + num2
-    return result
+    print(result)
+
+
 
 def subtract(num1,num2):
     result = num1 - num2
-    return result
+    print(result)
 
 def divide(num1,num2):
     result = num1/num2
-    return result
+    print(result)
 
 def multiply(num1,num2):
     result = num1*num2
-    return result
+    print(result)
+
+#check on operator
+if operator == "+":
+    add(number1,number2)
+elif operator == "-":
+    subtract(number1,number2)
+elif operator =="/":
+    divide(number1,number2)
+elif operator == "X" or operator == "X" or operator == "*":
+    multiply(number1,number2)
+else:
+    print("invalid operator")
