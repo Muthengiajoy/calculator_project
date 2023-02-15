4cbcfc320f4bd7fbc2bffd0f513294241903e550
Muthengiajoy <prof.joy7079@gmail.com>
2023-02-15 10:44:21 +0300

    git commit

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#this is a calculator project
+num1 = eval(input("enter_first_number:"))
+num2 = eval(input("enter_second_number:"))
+operator = input("enter_operator:")
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1,num2):
+    result = num1 - num2
+    return result
+
+def divide(num1,num2):
+    result = num1/num2
+    return result
+
+def multiply(num1,num2):
+    result = num1*num2
+    return result
